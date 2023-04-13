4b89c4eb71b4e4aa3672ffca279c97c2d7ab4cb2
AbdelwahabTop <mramged936@gmail.com>
2023-04-13 16:22:42 +0200

    update

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -89,15 +89,15 @@ export default class view {
 
   renderMessage(message = this._message) {
     const markup = `
-        <div class="message">
-          <div>
-            <svg>
-              <use href="${icons}#icon-smile"></use>
-            </svg>
-          </div>
-          <p>${message}</p>
-        </div> 
-      `;
+      <div class="message">
+        <div>
+          <svg>
+            <use href="${icons}#icon-smile"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+    `;
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
